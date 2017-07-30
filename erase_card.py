34f47afa478bbb6d898cb2f04ee7b45ae9aee938
shaun <wacko_racoon@yahoo.fr>
2017-07-30 20:28:56 +0000

    add erase script, modifications on CardList.py and box.py to support GPIO

---
+++ erase_card.py
@@ -0,0 +1,17 @@
+#from readtest import *
+from CardList import CardList
+from Reader import Reader
+reader = Reader()
+cardList = CardList()
+
+#while True:
+#        print 'Place the card in the reader'
+#        card = reader.readCard()
+#        plist=raw_input('Specify Spotify URI, q to quit')
+#        if plist=="q":
+#                break
+#	cardList.addPlaylist(card, plist)
+print "Place card to erase"
+card = reader.readCard()
+#print card
+cardList.erasePlaylist(card)
